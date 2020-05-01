f4aaece253960075d8302abebf575ccbd2c684d6
James McNair <booksaw224@gmail.com>
2020-05-01 10:48:11 +0100

    Vector Manipulation Methods

--- src/main/java/com/booksaw/Engine2D/Vector.java
+++ src/main/java/com/booksaw/Engine2D/Vector.java
@@ -110,4 +110,28 @@ public class Vector {
 		return Math.atan(y / x);
 	}
 
+	/**
+	 * Used to stop the object
+	 */
+	public void stop() {
+		x = 0;
+		y = 0;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
 }
